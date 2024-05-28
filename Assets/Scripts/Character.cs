e78b4c4d0db02f61e02ce857c06c3a67c54b61eb
jina1612 <jenny12161234@gmail.com>
2024-05-28 12:19:24 +0900

    캐릭터 어택

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,12 +4,37 @@ public class Character : MonoBehaviour
 {
     private Animator animator;
     private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rigidbody2d;
+    private AudioSource audioSource;
+
+    public AudioClip JumpClip;
 
     public float Speed = 4f;
+    public float JumpPower = 6f;
+    public AudioClip AttackClip;
+
+    private bool isFloor;
+
+    public GameObject AttackObj;
+    public float AttackSpeed = 3f;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
+    }
 
     private void Update()
     {
         Move();
+    }
+
+    private void FixedUpdate()
+    {
+        Jump();
+        Attack();
     }
     private void Move()
     {
@@ -17,11 +42,93 @@ public class Character : MonoBehaviour
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.Translate(Vector3.right * Speed * Time.deltaTime);
+            animator.SetBool("Move", true);
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Translate(Vector3.left * Speed * Time.deltaTime);
+            animator.SetBool("Move", true);
+        }
+        else
+        {
+            animator.SetBool("Move", false);
         }
 
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            spriteRenderer.flipX = true;
+        }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            isFloor = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            isFloor = false;
+        }
+    }
+
+    private void JumpCheck()
+    {
+        if (isFloor)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                justJump = true;
+            }
+        }
+    }
+    private void Jump()
+    {
+        if (justJump)
+        {
+            justJump = false;
+
+            rigidbody2d.AddForce(Vector2.up * JumpPower, ForceMode2D.Impulse);
+            animator.SetTrigger("Jump");
+            audioSource.PlayOneShot(JumpClip);
+        }
+    }
+    private void Attackcheck()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            justAttack = true;
+        }
+    }
+    private void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            animator.SetTrigger("Attack");
+
+            if (spriteRenderer.flipX)
+            {
+                GameObject obj = Instantiate(AttackObj, transform.position, Quaternion.Euler(0f, 180f, 0f));
+                obj.GetComponent<Rigidbody2D>().AddForce(Vector2.left * AttackSpeed.ForceMode2D.Impulse);
+                Destroy(obj, 3f);
+            }
+            else
+            {
+                GameObject obj = Instantiate(AttackObj, transform.position, Quaternion.Euler(0, 0, 0));
+                obj.GetComponent<Rigidbody2D>().AddForce(Vector2.right * AttackSpeed, ForceMode2D.Impulse);
+                Destroy(obj, 3f);
+            }
+        }
+    }
+
+
 }
+
